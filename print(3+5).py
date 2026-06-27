f2805319f8b0a90b80a229ee3c28d7b4cd0f01aa
koayxinchun4 <koayxinchun4@users.noreply.github.com>
2026-06-27 17:50:38 +0800

    update

---
+++ print(3+5).py
@@ -0,0 +1,21 @@
+##This is the variable
+a =4
+A ="sally"
+#A will not overwrite a
+print(a,A)
+
+#variable names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
